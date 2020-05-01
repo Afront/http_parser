9ab4fdc5a49fe9b53a0565c36facccf42ecdeac8
Afront <3943720+Afront@users.noreply.github.com>
2020-05-01 23:09:55 +0900

    Validate fields

--- src/http_parser.cr
+++ src/http_parser.cr
@@ -7,23 +7,29 @@ module HttpParser
     VERSIONS = Set{0.9, 1.0, 1.1, 2.0, 3.0}
 
     def initialize(http_version : String | Float, hash : Hash)
-      @http_version = validate_verison(http_version)
-      @hash = validate_hash(hash)
+      @http_version = validate_verison http_version
+      @hash = validate_hash hash
     end
 
     def validate_hash(hash : Hash)
     end
 
-    def validate_version(version : String)
-      raise ArgumentError if !VERSIONS.includes? version
+    def validate_version(version : String) : String
+      (raise ArgumentError if !VERSIONS.includes? version) || version
+      # raise ArgumentError if !VERSIONS.includes? version
+    end
+
+    def get(field : String | Symbol)
+      hash[field]
     end
   end
 
   class Response < Message
     property code
 
-    def initialize(http_version : String | Float, @hash : Hash)
-      @http_version = validate_verison(http_version)
+    def initialize(http_version : String | Float, hash : Hash)
+      @http_version = validate_verison http_version
+      @hash = validate_hash hash
     end
   end
 
@@ -32,12 +38,14 @@ module HttpParser
     HTTP_METHODS   = Set{:get, :heard, :post, :put, :delete, :connect, :options}
     HTCPCP_METHODS = Set{:brew, :propfind, :when}
 
-    def initialize(http_version : String | Float, method : String, @hash : Hash)
-      @http_version = validate_verison(http_version)
-      @method = validate_method(method.downcase.to_symbol)
+    def initialize(http_version : String | Float, method : String, hash : Hash)
+      @http_version = validate_verison http_version
+      @method = validate_method method.downcase.to_symbol
+      @hash = validate_hash hash
     end
 
-    def validate_method(method : Symbol)
+    def validate_method(method : Symbol) : String
+      (raise ArgumentError if !(HTTP_METHODS + HTCPCP_METHODS).includes? method) || method
       raise ArgumentError if !(HTTP_METHODS + HTCPCP_METHODS).includes? method
     end
   end
